def1c3ee2cf45f2b5803a07e034661ab83c632dd
Shawn Smith <shawnpsmith@gmail.com>
2021-01-15 14:54:59 +0900

    add color to score

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shawnps/go-awair-local
 
 go 1.15
+
+require github.com/fatih/color v1.10.0
--- main.go
+++ main.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"text/tabwriter"
 
+	"github.com/fatih/color"
 	"github.com/shawnps/go-awair-local/awair"
 )
 
@@ -24,11 +26,38 @@ func main() {
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
 
-	fmt.Fprintf(w, "Score\t%d\n", latestData.Score)
+	scoreStr := strconv.Itoa(latestData.Score)
+
+	fmt.Fprintf(w, "Score\t%s\n", colorPrintFn("Score", latestData.Score)(scoreStr))
 	fmt.Fprintf(w, "Temp\t%.2f\n", latestData.Temp)
 	fmt.Fprintf(w, "Humidity\t%.2f\n", latestData.Humid)
 	fmt.Fprintf(w, "CO2\t%d\n", latestData.CO2)
 	fmt.Fprintf(w, "VOC\t%d\n", latestData.VOC)
 	fmt.Fprintf(w, "PM2.5\t%d\n", latestData.PM25)
 	w.Flush()
+}
+
+// https://support.getawair.com/hc/en-us/articles/360039242373-Air-Quality-Factors-Measured-By-Awair-Element#h_9c45cca9-012c-4419-ac2c-d3afeb683351
+func colorPrintFn(factor string, value interface{}) func(a ...interface{}) string {
+	green := color.New(color.FgGreen).SprintFunc()
+	yellow := color.New(color.FgYellow).SprintFunc()
+	magenta := color.New(color.FgMagenta).SprintFunc()
+	red := color.New(color.FgRed).SprintFunc()
+
+	switch factor {
+	case "Score":
+		v := value.(int)
+		switch {
+		case v >= 90:
+			return green
+		case v >= 80:
+			return yellow
+		case v >= 60:
+			return magenta
+		default:
+			return red
+		}
+	}
+
+	return color.New(color.FgYellow).SprintFunc()
 }

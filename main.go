ca3b76cf32ef5585d9678ce3202e8b98a0c9c9e2
Shawn Smith <shawnpsmith@gmail.com>
2021-01-15 15:20:13 +0900

    add more color

--- main.go
+++ main.go
@@ -31,13 +31,18 @@ func main() {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
 
 	scoreStr := strconv.Itoa(latestData.Score)
+	tempStr := fmt.Sprintf("%.2f", latestData.Temp)
+	humidStr := fmt.Sprintf("%.2f", latestData.Humid)
+	co2Str := strconv.Itoa(latestData.CO2)
+	vocStr := strconv.Itoa(latestData.VOC)
+	pm25Str := strconv.Itoa(latestData.PM25)
 
 	fmt.Fprintf(w, "Score\t%s\n", colorPrintFn("Score", latestData.Score)(scoreStr))
-	fmt.Fprintf(w, "Temp\t%.2f\n", latestData.Temp)
-	fmt.Fprintf(w, "Humidity\t%.2f\n", latestData.Humid)
-	fmt.Fprintf(w, "CO2\t%d\n", latestData.CO2)
-	fmt.Fprintf(w, "VOC\t%d\n", latestData.VOC)
-	fmt.Fprintf(w, "PM2.5\t%d\n", latestData.PM25)
+	fmt.Fprintf(w, "Temp\t%s\n", colorPrintFn("Temp", latestData.Temp)(tempStr))
+	fmt.Fprintf(w, "Humidity\t%s\n", colorPrintFn("Humid", latestData.Humid)(humidStr))
+	fmt.Fprintf(w, "CO2\t%s\n", colorPrintFn("CO2", latestData.CO2)(co2Str))
+	fmt.Fprintf(w, "VOC\t%s\n", colorPrintFn("VOC", latestData.VOC)(vocStr))
+	fmt.Fprintf(w, "PM2.5\t%s\n", colorPrintFn("PM25", latestData.PM25)(pm25Str))
 	w.Flush()
 }
 
@@ -57,6 +62,74 @@ func colorPrintFn(factor string, value interface{}) func(a ...interface{}) strin
 		case v >= 80:
 			return yellow
 		case v >= 60:
+			return magenta
+		default:
+			return red
+		}
+	case "Temp":
+		v := value.(float64)
+		switch {
+		case v >= 18 && v <= 25:
+			return green
+		case v >= 25 && v <= 26:
+			return yellow
+		case v >= 17 && v <= 18:
+			return yellow
+		case v >= 9 && v <= 17:
+			return magenta
+		case v >= 26 && v <= 34:
+			return magenta
+		default:
+			return red
+		}
+	case "Humid":
+		v := value.(float64)
+		switch {
+		case v >= 40 && v <= 50:
+			return green
+		case v >= 35 && v <= 40:
+			return yellow
+		case v >= 50 && v <= 60:
+			return yellow
+		case v >= 60 && v <= 80:
+			return magenta
+		case v >= 15 && v <= 35:
+			return magenta
+		default:
+			return red
+		}
+	case "CO2":
+		v := value.(int)
+		switch {
+		case v <= 600:
+			return green
+		case v <= 1000:
+			return yellow
+		case v <= 1500:
+			return magenta
+		default:
+			return red
+		}
+	case "VOC":
+		v := value.(int)
+		switch {
+		case v >= 0 && v <= 333:
+			return green
+		case v >= 333 && v <= 1000:
+			return yellow
+		case v >= 1000 && v <= 8332:
+			return magenta
+		default:
+			return red
+		}
+	case "PM25":
+		v := value.(int)
+		switch {
+		case v >= 0 && v <= 15:
+			return green
+		case v >= 15 && v <= 35:
+			return yellow
+		case v >= 35 && v <= 75:
 			return magenta
 		default:
 			return red

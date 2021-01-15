bf8105ca925da53ef7278b939e8acb49c67a20b7
Shawn Smith <shawnpsmith@gmail.com>
2021-01-15 14:23:17 +0900

    tabulate command output

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"text/tabwriter"
 
 	"github.com/shawnps/go-awair-local/awair"
 )
@@ -20,6 +22,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(latestData)
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.Debug)
 
+	fmt.Fprintf(w, "Score\t%d\n", latestData.Score)
+	fmt.Fprintf(w, "Temp\t%f\n", latestData.Temp)
+	fmt.Fprintf(w, "Humidity\t%f\n", latestData.Humid)
+	fmt.Fprintf(w, "CO2\t%d\n", latestData.CO2)
+	fmt.Fprintf(w, "VOC\t%d\n", latestData.VOC)
+	fmt.Fprintf(w, "PM2.5\t%d\n", latestData.PM25)
+	w.Flush()
 }
